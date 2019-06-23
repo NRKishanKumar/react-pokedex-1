8e08b1677ffeec21ba74b4e08a7119ba66bb00d9
Kishan Kumar <kishan.kumar@intimetec.com>
2019-06-23 22:50:37 +0530

    Updates

---
+++ precache-manifest.f9ab13bd6ec8a2f6806afef5c9bc81bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e8952c1abcc3c0e5ca2ef4e0895a01",
+    "url": "/NRKishanKumar/react-pokedex-1.git/index.html"
+  },
+  {
+    "revision": "a497a8f543b33a3e6b88",
+    "url": "/NRKishanKumar/react-pokedex-1.git/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dd082ec3880257a8e23c",
+    "url": "/NRKishanKumar/react-pokedex-1.git/static/css/main.132a93b0.chunk.css"
+  },
+  {
+    "revision": "a497a8f543b33a3e6b88",
+    "url": "/NRKishanKumar/react-pokedex-1.git/static/js/2.a07dbb4a.chunk.js"
+  },
+  {
+    "revision": "dd082ec3880257a8e23c",
+    "url": "/NRKishanKumar/react-pokedex-1.git/static/js/main.f829172a.chunk.js"
+  },
+  {
+    "revision": "73cba1bc88e79688e679",
+    "url": "/NRKishanKumar/react-pokedex-1.git/static/js/runtime~main.9c332953.js"
+  },
+  {
+    "revision": "964c12ef9060e0555f4901420fc6473d",
+    "url": "/NRKishanKumar/react-pokedex-1.git/static/media/pattern.964c12ef.png"
+  }
+]);
